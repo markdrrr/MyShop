0194d62f210f4aee4bd4b6719390fb5dc34c2d31
markdrrr <markdrrr@gmail.com>
2020-11-22 18:22:15 +0300

    refactoring

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path, include
 from .views import *
-
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('', BaseView.as_view(), name='base'),
-    path('products/<str:ct_model>/<str:slug>/', product_detail_view, name='product_detail'),
+    path('products/<str:slug>/', product_detail_view, name='product_detail'),
     path('category/<str:slug>/', CategoryDetailView.as_view(), name='category_detail'),
     path('cart/', CartView.as_view(), name='cart'),
-    path('add-to-cart/<str:ct_model>/<str:slug>/', AddToCartView.as_view(), name='add_to_cart'),
-    path('delete-from-cart/<str:ct_model>/<str:slug>/', DeleteFromCartView.as_view(), name='delete_from_cart'),
-    path('change-count/<str:ct_model>/<str:slug>/', ChangeCountView.as_view(), name='change_count'),
+    path('add-to-cart/<str:slug>/', AddToCartView.as_view(), name='add_to_cart'),
+    path('delete-from-cart/<str:slug>/', DeleteFromCartView.as_view(), name='delete_from_cart'),
+    path('change-count/<str:slug>/', ChangeCountView.as_view(), name='change_count'),
     path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('make-order/', MakeOrderView.as_view(), name='make_order'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('registration/', RegistrationView.as_view(), name='registration'),
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
